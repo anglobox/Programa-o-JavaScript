d30d7bca796e08bf209a348b3a680d4b9230fa46
Angelo_Camargo <anglobox21@gmail.com>
2025-05-19 21:27:07 -0300

    corrigindo-formatacao

--- variavel-ex6-7-8-9-10.js
+++ variavel-ex6-7-8-9-10.js
@@ -58,4 +58,4 @@ let salario = 1500
 let diasTrabalhados = 22
 let horasTrabalhadas = 8
 let valorHora = (salario / diasTrabalhados) / horasTrabalhadas
-console.log("Um trabalhador recebe R$ 1.500,00 por mês e trabalha 8 horas por dia, 22 dias por mês. Calcule o valor recebido por hora trabalhada.", valorHora)
+console.log("Um trabalhador recebe R$ 1.500,00 por mês e trabalha 8 horas por dia, 22 dias por mês. Calcule o valor recebido por hora trabalhada.", valorHora.toFixed(2))
