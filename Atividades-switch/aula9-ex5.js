75cc6ecee9042f32a5600cf158a8002c4f385952
Angelo_Camargo <anglobox21@gmail.com>
2025-06-30 18:55:36 -0300

    tentativa-das-atividades

---
+++ Atividades-switch/aula9-ex5.js
@@ -0,0 +1,7 @@
+let n = Number(prompt("Vamos jogar adivininhe um numero de 1 a 10: "))
+while (n != 4) {
+    console.log("Numero incorreto")
+if (n == 4)
+    console.log("Você acertou!!!")
+break
+}
